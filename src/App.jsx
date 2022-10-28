f3e8825bdaa94f050442abb96aece596a9f23bf2
Joshua Burke <burke.joshua.james@gmail.com>
2022-10-27 20:09:07 -0400

    Make the first name input effect the message when changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [firstName, setFirstName] = useState('Default Value...');
+  const [middleName, setMiddleName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  function handleChangeFirstName(event) {
+    setFirstName(event.target.value);
+  }
+
   return (
-    <p>Hello, JavaScript!</p>
+    <>
+      <p>Hello, { firstName } you are awesome!</p>
+      <form>
+        <label htmlFor="first-name">First Name: </label>
+        <input
+          id="first-name"
+          type="text"
+          name="first-name"
+          onInput={handleChangeFirstName}
+          value={ firstName }
+        />
+      </form>
+    </>
   );
 }
 
